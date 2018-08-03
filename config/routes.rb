16d621c3311c3b950d157801e8c6260ba8066334
YosukeFujii19910222 <pas46492>
2018-08-03 23:41:01 +0900

    Routing Create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'trainer/new'
-
+  get 'trainer/new' => 'trainer#new'
+  post 'trainer/create' => 'trainer#create'
   get '/' => 'home#top'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
