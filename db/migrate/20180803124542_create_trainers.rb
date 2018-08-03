0dd382ceeaee2070bc2b3adc728487c051b260f0
YosukeFujii19910222 <pas46492>
2018-08-03 22:00:11 +0900

    Define Trainer column

--- db/migrate/20180803124542_create_trainers.rb
+++ db/migrate/20180803124542_create_trainers.rb
@@ -3,7 +3,13 @@ class CreateTrainers < ActiveRecord::Migration[5.0]
     create_table :trainers do |t|
       t.string :name
       t.string :email
-
+      t.string :password
+      t.string :speciality
+      t.string :gender
+      t.string :height
+      t.string :weight
+      t.string :age
+      t.text :career
       t.timestamps
     end
   end
