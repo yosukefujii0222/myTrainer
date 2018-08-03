cfed3ac508ca5d7413beddaed8fb19d78b9fe60e
YosukeFujii19910222 <pas46492>
2018-08-03 23:40:18 +0900

    Define create action

--- app/controllers/trainer_controller.rb
+++ app/controllers/trainer_controller.rb
@@ -1,4 +1,25 @@
 class TrainerController < ApplicationController
   def new
   end
+
+  def create
+    @trainer = Trainer.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      speciality: params[:speciality],
+      gender: params[:gender],
+      height: params[:height],
+      weight: params[:weight],
+      age: params[:age],
+      career: params[:career]
+    )
+    if params[:image]
+      @trainer.image = "#{@trainer.email}.jpg"
+      imageData = params[:image]
+      File.binwrite("public/trainer_images/#{@trainer.image}", imageData.read)
+    end
+    @trainer.save
+    redirect_to("/")
+  end
 end
