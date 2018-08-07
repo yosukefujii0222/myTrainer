beb255b5d59bf96c4d3616afdd35397d57f506b5
YosukeFujii19910222 <pas46492>
2018-08-07 20:12:21 +0900

    Routing trainer-show

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   post 'trainer/create' => 'trainer#create'
   get 'trainer/trainerLogin' => 'trainer#trainerLogin'
   post 'trainerLogin' => 'trainer#login'
-  post "trainer/logout" => "trainer#logout"
+  post 'trainer/logout' => 'trainer#logout'
+  get 'trainer/:id' => 'trainer#show'
   get '/' => 'home#top'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
