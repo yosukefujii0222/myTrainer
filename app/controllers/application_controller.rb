43a837af34fd9c16ff5435a15da44bf59188f093
YosukeFujii19910222 <pas46492>
2018-08-07 09:33:52 +0900

    Before action set current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_current_user
+
+  def set_current_user
+    if session[:user_id]
+      @current_user = User.find_by(id: session[:user_id])
+    elsif session[:trainer_id]
+      @current_user = Trainer.find_by(id: session[:trainer_id])
+    end
+  end
 end
