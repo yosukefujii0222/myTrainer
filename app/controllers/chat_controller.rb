a150085cb0f8df4e3482e8a904d3205e527cad2f
YosukeFujii19910222 <pas46492>
2018-08-11 19:19:25 +0900

    Code chat-controller

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,2 +1,20 @@
 class ChatController < ApplicationController
+  def show
+    @chats = Chat.where(follower_id: params[:room_id])
+    @follower = Follower.find_by(id: params[:room_id])
+  end
+
+  def trainerCreate
+    @chat = Chat.new(sender: 1, body: params[:body], follower_id: params[:room_id])
+    @chat.save
+    redirect_to("/chat/#{params[:room_id]}")
+  end
+
+  def userCreate
+    @chat = Chat.new(sender: 2, body: params[:body], follower_id: params[:room_id])
+    @chat.save
+    redirect_to("/chat/#{params[:room_id]}")
+  end
+
+
 end
