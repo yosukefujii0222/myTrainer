fc7a62bf6347a70147a6f1f0e3045642c3439343
YosukeFujii19910222 <pas46492>
2018-08-11 16:17:31 +0900

    Chat-database migrate

--- db/migrate/20180811071016_create_chats.rb
+++ db/migrate/20180811071016_create_chats.rb
@@ -1,7 +1,8 @@
 class CreateChats < ActiveRecord::Migration[5.0]
   def change
     create_table :chats do |t|
-
+      t.text :body
+      t.integer :follower_id
       t.timestamps
     end
   end
